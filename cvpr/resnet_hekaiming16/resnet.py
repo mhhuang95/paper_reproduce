2fe69f7943fd2dd9262a5f5119ab49aa8c58e267
mhhuang95 <mhhuang@ucdavis.edu>
2019-09-07 10:43:46 +0800

    resnet archi

--- cvpr/resnet_hekaiming16/resnet.py
+++ cvpr/resnet_hekaiming16/resnet.py
@@ -1,5 +1,6 @@
 # Minhui huang
 # Reproducing ResNet --cvpr16
+#-----------------------------------------------------------------------------------
 
 
 import numpy as np
@@ -77,7 +78,7 @@ def conv_bn_relu_layer(input_layer, filter_shape, stride):
     out_channel = filter_shape[-1]
     filter = create_variables(name='conv', shape=filter_shape)
 
-    conv = tf.nn.conv_2d(input_layer, filter, stride=[1,stride, stride, 1], padding='SAME')
+    conv = tf.nn.conv_2d(input_layer, filter, strides=[1,stride, stride, 1], padding='SAME')
     bn_layer = batch_nomalization_layer(conv, out_channel)
 
     output = tf.nn.relu(bn_layer)
@@ -98,7 +99,7 @@ def bn_relu_conv_layer(input_layer, filter_shape, stride):
     relu_layer = tf.nn.relu(bn_layer)
 
     filter = create_variables(name='conv', shape=filter_shape)
-    conv = tf.nn.conv_2d(relu_layer, filter, stride=[1,stride, stride, 1], padding='SAME')
+    conv = tf.nn.conv_2d(relu_layer, filter, strides=[1,stride, stride, 1], padding='SAME')
 
     return conv
 
@@ -126,7 +127,7 @@ def residual_block(input_layer, output_channel, first_block=False):
     with tf.variable_scope('conv1_in_block'):
         if first_block:
             filter = create_variables(name='conv', shape=[3,3,input_channel,output_channel])
-            conv1 = tf.nn.conv_2d(input_layer, filter=filter, stride=[1,1,1,1], padding='SAME')
+            conv1 = tf.nn.conv_2d(input_layer, filter=filter, strides=[1,1,1,1], padding='SAME')
         else:
             conv1 = bn_relu_conv_layer(input_layer, [3,3,input_channel, output_channel], stride)
 
@@ -134,4 +135,76 @@ def residual_block(input_layer, output_channel, first_block=False):
         conv2 = bn_relu_conv_layer(conv1, [3,3,output_channel, output_channel], 1)
 
     if increase_dim:
-        
+        pooled_input = tf.nn.avg_pool(input_layer, ksize=[1,2,2,1], strides=[1,2,2,1], padding='VALID')
+        pooled_input = tf.pad(pooled_input, [[0, 0], [0, 0], [0, 0], [input_channel // 2, input_channel // 2]])
+
+    else:
+        pooled_input = input_layer
+
+    output = conv2 + pooled_input
+
+    return output
+
+
+
+def inference(input_tensor_batch, n, reuse):
+    '''
+
+    :param input_tensor_batch: 4D tensor
+    :param n: num_residual_blocks
+    :param reuse: To build train graph, reuse=False. To build validation graph and share weights
+    with train graph, resue=True
+    :return: last layer in the network.
+    '''
+
+    layers = []
+    with tf.variable_scope('conv0', reuse=reuse):
+        conv0 = conv_bn_relu_layer(input_tensor_batch, [3,3,3,16], 1)
+        activation_summary(conv0)
+        layers.append(conv0)
+
+    for i in range(n):
+        with tf.variable_scope('conv1_%d' %i, reuse=reuse):
+            if i == 0:
+                conv1 = residual_block(layers[-1], 16, first_block=True)
+            else:
+                conv1 = residual_block(layers[-1], 16)
+            activation_summary(conv1)
+            layers.append(conv1)
+
+    for i in range(n):
+        with tf.variable_scope('conv2_%d' % i, reuse=reuse):
+            conv2 = residual_block(layers[-1], 32)
+            activation_summary(conv2)
+            layers.append(conv2)
+
+    for i in range(n):
+        with tf.variable_scope('conv3_%d' % i, reuse=reuse):
+            conv3 = residual_block(layers[-1], 64)
+            layers.append(conv3)
+        assert conv3.get_shape().as_list()[1:] == [8, 8, 64]
+
+    with tf.variable_scope('fc', reuse=reuse):
+        in_channel = layers[-1].get_shape().as_list()[-1]
+        bn_layer = batch_nomalization_layer(layers[-1], in_channel)
+        relu_layer = tf.nn.relu(bn_layer)
+        global_pool = tf.reduce_mean(relu_layer, [1,2])
+
+        assert global_pool.get_shape().as_list()[-1:] == [64]
+        output = output_layer(global_pool, 10)
+        layers.append(output)
+
+    return layers[-1]
+
+def test_graph(train_dir='logs'):
+    '''
+    Run this function to look at the graph structure on tensorboard. A fast way!
+    :param train_dir:
+    :return:
+    '''
+    input_tensor = tf.constant(np.ones([128, 32, 32, 3]), dtype=tf.float32)
+    result = inference(input_tensor, 2, reuse=False)
+    init = tf.initialize_all_variables()
+    sess = tf.Session()
+    sess.run(init)
+    summary_writer = tf.train.SummaryWriter(train_dir, sess.graph)
